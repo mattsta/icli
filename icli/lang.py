8204033654cc1210829152fed0c088104162743e
Matt Stancliff <matt@genges.com>
2021-11-23 21:45:34 -0800

    Add "cash" command for margin math overview
    returns a table of cash, overnight margin, intraday margin
    plus also does some "buffer math" for maximum safe spending.

--- icli/lang.py
+++ icli/lang.py
@@ -267,6 +267,44 @@ class IOpPositionEvict(IOp):
 
 
 @dataclass
+class IOpCash(IOp):
+    def argman(self):
+        return []
+
+    async def run(self):
+        result = {
+            "Avail Full": [
+                self.state.accountStatus["AvailableFunds"],
+                self.state.accountStatus["AvailableFunds"] * 2,
+                self.state.accountStatus["AvailableFunds"] * 4,
+            ],
+            "Avail Buffer": [
+                self.state.accountStatus["AvailableFunds"] / 1.10,
+                self.state.accountStatus["AvailableFunds"] * 2 / 1.10,
+                self.state.accountStatus["AvailableFunds"] * 4 / 1.10,
+            ],
+            "Net Full": [
+                self.state.accountStatus["NetLiquidation"],
+                self.state.accountStatus["NetLiquidation"] * 2,
+                self.state.accountStatus["NetLiquidation"] * 4,
+            ],
+            "Net Buffer": [
+                self.state.accountStatus["NetLiquidation"] / 1.10,
+                self.state.accountStatus["NetLiquidation"] * 2 / 1.10,
+                self.state.accountStatus["NetLiquidation"] * 4 / 1.10,
+            ],
+        }
+
+        printFrame(
+            pd.DataFrame.from_dict(
+                {k: [f"${v:,.2f}" for v in vv] for k, vv in result.items()},
+                orient="index",
+                columns=["Cash", "Overnight", "Day"],
+            )
+        )
+
+
+@dataclass
 class IOpDepth(IOp):
     def argmap(self):
         return [
@@ -1368,6 +1406,7 @@ OP_MAP = {
         "try": None,
         "tryf": None,
         "snd": IOpSound,
+        "cash": IOpCash,
     },
 }
 
