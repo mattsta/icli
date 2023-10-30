b7c881da4b1494c04b3a5b139d6f1b493d802f30
Matt Stancliff <matt@genges.com>
2023-10-29 20:19:14 -0700

    Add 'range' helper to print OCC strikes to a file
    Our algo systems consume OCC strikes and this is an easy way to generate
    a wide number of strikes easily.
    
    It's useful having this in icli instead of only the algo agents because
    the algo agents only have quote data without knowing the underlying
    prices, but here we want to generate a ± point range from the current
    prices, so here in icli we have both prices and strike details, so we
    can dump it all at once.

--- icli/lang.py
+++ icli/lang.py
@@ -7,6 +7,7 @@ import enum
 
 import fnmatch
 import math
+import pathlib
 
 import sys
 from collections import Counter, defaultdict
@@ -2655,6 +2656,95 @@ class IOpOrderSpread(IOp):
 
 
 @dataclass
+class IOpOptionChainRange(IOp):
+    """Print OCC symbols for an underlying with ± offset for puts and calls."""
+
+    def argmap(self):
+        # TODO: add options to allow multiple symbols and also printing cached chains
+        return [
+            DArg("symbol", convert=lambda x: x.upper()),
+            DArg("width", convert=lambda x: float(x)),
+            DArg(
+                "*rename",
+                convert=lambda x: x[0].upper() if x else None,
+                desc="Optionally rename the generated strike with a different symbol name (e.g. 'range SPX 20 SPXW')",
+            ),
+        ]
+
+    async def run(self):
+        # verify we have quotes enabled to get the current price...
+        await self.runoplive("add", f'"{self.symbol}"')
+
+        datestrikes = await self.runoplive(
+            "chains",
+            self.symbol,
+        )
+
+        quote = self.state.quoteState[self.symbol]
+
+        # prices are NaN until they get populated...
+        # (and sometimes this doesn't work right after hours or weekend times... thanks ibkr)
+        # TODO: we could _also_ check the bid/ask spread if the prices aren't populating.
+        while (quote.last != quote.last) or (quote.close != quote.close):
+            logger.info("[{}] Waiting for price to populate...", self.symbol)
+            await asyncio.sleep(0.25)
+
+        # this is a weird way of masking out all NaN values leaving only the good values, so we select
+        # the first non-NaN value in this order here...
+        fetchone = np.array([quote.last, quote.close])
+        currentPrice = fetchone[np.isfinite(fetchone)][0]
+
+        low = currentPrice - self.width
+        high = currentPrice + self.width
+        logger.info(
+            "[{} :: {}] Providing option chain range between [{}, {}] using current price {:,.2f}",
+            self.symbol,
+            self.width,
+            low,
+            high,
+            currentPrice,
+        )
+
+        today = str(pendulum.now("US/Eastern").date()).replace("-", "")
+        generated = []
+
+        # we need to sort by the date values since the IBKR API returns dates in a random order...
+        for date, strikes in sorted(
+            datestrikes[self.symbol].items(), key=lambda x: x[0]
+        ):
+            # don't generate strikes in the past...
+            if date < today:
+                logger.warning("[{}] Skipping date because it's in the past...", date)
+                continue
+
+            logger.info("[{}] Using date as basis for strike generation...", date)
+            for strike in strikes:
+                if low < strike < high:
+                    for pc in ("P", "C"):
+                        generated.append(
+                            "{}{}{}{:0>8}".format(
+                                self.rename or self.symbol,
+                                date[2:],
+                                pc,
+                                int(strike * 1000),
+                            )
+                        )
+
+            # only print for THE FIRST DATE FOUND
+            # (so we don't end up writing these out for every daily expiration or
+            #  an entire month of expirations, etc)
+            break
+
+        for row in generated:
+            logger.info("Got: {}", row)
+
+        out = f"strikes-{self.rename or self.symbol}.txt"
+        pathlib.Path(out).write_text("\n".join(generated) + "\n")
+
+        logger.info("[{} :: {}] Saved strikes to: {}", self.symbol, self.width, out)
+
+
+@dataclass
 class IOpOptionChain(IOp):
     """Print option chains for symbol"""
 
@@ -2993,6 +3083,7 @@ OP_MAP = {
         "oadd": IOpQuotesAddFromOrderId,
         "remove": IOpQuotesRemove,
         "chains": IOpOptionChain,
+        "range": IOpOptionChainRange,
     },
     "Order Management": {
         "limit": IOpOrderLimit,
