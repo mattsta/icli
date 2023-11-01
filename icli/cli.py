a84a4f4b4ecfab6d56dd5504b3142ebc49ec31ee
Matt Stancliff <matt@genges.com>
2023-11-01 07:19:11 -0700

    Cleanup order preview output a little more
    We had a copy/paste error where maint was equal to initial by mistake.
    
    Also don't condition the multiplier usage on only futures because other
    fun things like forex can have multipliers too.
    
    Also fix a formatting annoyance in the "not enough funds" warning to
    just look better.

--- icli/cli.py
+++ icli/cli.py
@@ -699,9 +699,7 @@ class IBKRCmdlineApp:
                 # Also note: there is _something_ off with our math because we aren't getting exactly 30% or 25% or 3% or 5% etc,
                 #            but it's close enough for what we're trying to show at this point.
 
-                multiplier = (
-                    float(contract.multiplier) if isinstance(contract, Future) else 1
-                )
+                multiplier = float(contract.multiplier or 1)
 
                 margPctInit = (
                     float(trade.initMarginChange)
@@ -709,7 +707,7 @@ class IBKRCmdlineApp:
                 ) * 100
 
                 margPctMaint = (
-                    float(trade.initMarginChange)
+                    float(trade.maintMarginChange)
                     / (order.totalQuantity * order.lmtPrice * multiplier)
                 ) * 100
 
@@ -733,7 +731,7 @@ class IBKRCmdlineApp:
                 excess = float(trade.equityWithLoanAfter) - float(trade.initMarginAfter)
                 if excess < 0:
                     logger.warning(
-                        "[{}] TRADE NOT VIABLE; MISSING EQUITY: ${:,.2f}",
+                        "[{}] TRADE NOT VIABLE. MISSING EQUITY: ${:,.2f}",
                         contract.localSymbol,
                         excess,
                     )
