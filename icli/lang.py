36eee665df6d1ac95cc91fde76786cbd3c78a207
Matt Stancliff <matt@genges.com>
2024-07-31 19:10:12 -0700

    Add per-instrument trade tick to detail output
    So _this_ is where IBKR hides the details like when options trade $0.01
    under $3 then $0.05 over $3... hadn't noticed this data was hiding in
    this place before.
    
    We could still optimize these lookups and cache more of these fields so
    we don't need network requests for the same data on each detail request,
    but the data is at least available and easy to read for now.

--- icli/lang.py
+++ icli/lang.py
@@ -614,9 +614,60 @@ class IOpDetails(IOp):
         # TODO: we should actually cache these detail results and have them expire at the end of every
         #       day (the details include day-changing quantities like next N day lookahead trading sessions,
         #       so the details _do_ change over time, but they _do not_ change within a single day).
+
+        # Map of RuleId to RuleValue
+        ruleCache: dict[int, ib_async.objects.PriceIncrement] = dict()
+
         for contract in contracts:
             try:
                 (detail,) = await self.ib.reqContractDetailsAsync(contract)
+
+                # IBKR "rules" map one marketRuleId to one exchange by position in each list.
+                # So even though detail shows like "26,26,26,26,..." 16 times it's because it matches 16 exchanges.
+                # Also see: https://interactivebrokers.github.io/tws-api/minimum_increment.html
+
+                # For cleaner results, we show the inverse of which rule id is serviced by which exchanges
+                # because the rules are primarily by instrument type, so almost all exchanges have the same rules.
+
+                # These "marketRuleIds" show the actual security increments like when options
+                # trade $0.01 under $3 then $0.05 over $3 versus $0.05 under $3 then $0.10 over $3 versus $0.01 for all, etc.
+
+                # split rule id string into integers
+                ridsAll = [int(x) for x in detail.marketRuleIds.split(",")]
+
+                # map exchanges to their matching rule ids
+                exchanges = detail.validExchanges.split(",")
+                exchangeRuleMapping = dict(zip(exchanges, ridsAll))
+
+                # fetch non-duplicate rule ids
+                # TODO: we should cache these lookup results forever. The underlying marketRuleId results will never change,
+                #       so running a network call for each detail attempt it wasteful.
+                rids = tuple(set(ridsAll))
+                rules = await asyncio.gather(
+                    *[self.ib.reqMarketRuleAsync(rid) for rid in rids]
+                )
+
+                # map rule ids to result value from lookup
+                ruleCache.update(dict(zip(rids, rules)))
+
+                # logger.info("Got rules of:\n{}", pp.pformat(rules))
+                # logger.info("Valid Exchanges: {}", detail.validExchanges)
+
+                # logger.info("Exchange rules: {} (via {})", exchangeRuleMapping, exchanges)
+
+                # FORWARD map of xchange -> rule
+                # (we conver the rule list to a tuple so it can then be a dict key when we invert this next)
+                exchangesWithRules = {
+                    xchange: tuple(ruleCache[rid])
+                    for xchange, rid in exchangeRuleMapping.items()
+                }
+
+                # REVERSE COLLECTIVE MAP of rule -> exchanges
+                rulesWithExchanges = defaultdict(list)
+                for xch, rule in exchangesWithRules.items():
+                    rulesWithExchanges[rule].append(xch)
+
+                # logger.info("Exchanges with rules: {}", exchangesWithRules)
             except:
                 logger.error("Contract details not found for: {}", contract)
                 continue
@@ -625,10 +676,17 @@ class IOpDetails(IOp):
             logger.info(
                 "[{}] Details: {}", detail.contract.localSymbol, pp.pformat(detail)
             )
+
             logger.info(
                 "[{}] Trading Sessions: {}",
                 detail.contract.localSymbol,
                 pp.pformat(detail.tradingSessions()),
+            )
+
+            logger.info(
+                "[{}] Exchange Rule Pairs:\n{}",
+                detail.contract.localSymbol,
+                pp.pformat(dict(rulesWithExchanges)),
             )
 
 
